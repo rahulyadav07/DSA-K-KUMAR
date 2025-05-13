8bf93a8d9b63f8e1ece508e0559626411bc2b567
rahulyadav <dk283532@gmail.com>
2025-05-13 09:26:37 +0530

    complete min and max freq question

---
+++ src/Hashing/FindMinAndMaxFreqElementInArray.java
@@ -0,0 +1,38 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FindMinAndMaxFreqElementInArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int arr[] = new int[s.nextInt()];
+
+        for(int i =0;i<arr.length;i++){
+            arr[i] = s.nextInt();
+        }
+
+        findMinMaxFrq(arr);
+    }
+    private static void findMinMaxFrq(int arr[]) {
+        int maxFre=0, maxValue=0, minFre=Integer.MAX_VALUE, minValue =0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int value:arr) {
+           map.put(value, map.getOrDefault(value, 0)+1);
+           if(maxFre<map.get(value)) {
+               maxFre = map.get(value);
+               maxValue = value;
+
+           }
+           if (minFre>map.get(value) || map.get(value) == 1) {
+               minValue = value;
+               minFre = map.get(value);
+           }
+        }
+
+        System.out.println(" Min Freq and Min Value " + minFre + " " +minValue);
+        System.out.print(" Max Freq and Max Value "  + maxFre + " " +maxValue);
+    }
+}
